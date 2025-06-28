0d81a58fdb951799ef0275cc841e299324e0ae7c
Aadarsh Agarwal <140622088+aadarshagarwal1@users.noreply.github.com>
2025-06-28 20:50:35 +0530

    Update redisClient.js

--- utils/redisClient.js
+++ utils/redisClient.js
@@ -1,5 +1,16 @@
-import Redis from "ioredis";
+import { createClient } from "redis";
 
-const client = new Redis(process.env.REDIS_SERVER_URL);
+const client = createClient({
+  username: "default",
+  password: process.env.REDIS_CLOUD_PASSWORD,
+  socket: {
+    host: process.env.REDIS_CLOUD_HOST,
+    port: process.env.REDIS_CLOUD_PORT,
+  },
+});
+
+client.on("error", (err) => console.log("Redis Client Error", err));
+
+await client.connect();
 
 export default client;
